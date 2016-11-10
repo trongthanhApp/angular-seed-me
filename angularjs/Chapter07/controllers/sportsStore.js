25f053cc706b5f52b0bad2ca25086ed153a39061
trong.nguyen <trongthanh.nguyenmail@yahoo.fr>
2016-11-10 16:09:41 +0100

    chapter 7 change port deployd

--- angularjs/Chapter07/controllers/sportsStore.js
+++ angularjs/Chapter07/controllers/sportsStore.js
@@ -1,5 +1,5 @@
 ﻿angular.module("sportsStore")
-    .constant("dataUrl", "http://localhost:5500/products")
+    .constant("dataUrl", "http://localhost:2403/products")
     .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
 
         $scope.data = {};
