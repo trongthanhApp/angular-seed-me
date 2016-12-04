c4700a614695ad208a957f32af4823e273263e43
nguyen <trongthanh.nguyenmail@yahoo.fr>
2016-12-04 19:02:08 +0100

    commit chapter 9

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 var appConnect = connect();
-appConnect.use(serveStatic("./angularjs/Chapter15"));
+appConnect.use(serveStatic("./angularjs/Chapter11"));
 appConnect.listen(5000);
 
 
