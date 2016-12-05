6d348b3a305eb3816699c32afeccb1b69a275f28
trong.nguyen <trongthanh.nguyenmail@yahoo.fr>
2016-12-05 15:53:08 +0100

    add chapter 17

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 var appConnect = connect();
-appConnect.use(serveStatic("./angularjs/Chapter18"));
+appConnect.use(serveStatic("./angularjs/Chapter16"));
 appConnect.listen(5000);
 
 
