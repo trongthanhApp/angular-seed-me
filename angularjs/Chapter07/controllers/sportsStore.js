f8f99b1ee8d663a868b7ad808b194297c05ffec4
nguyen <trongthanh.nguyenmail@yahoo.fr>
2017-01-29 00:48:01 +0100

    clean router to js

--- angularjs/Chapter07/controllers/sportsStore.js
+++ angularjs/Chapter07/controllers/sportsStore.js
@@ -1,4 +1,29 @@
-﻿angular.module("sportsStore")
+﻿
+angular.module("sportsStore", ["customFilters", "cart", "ngRoute"])
+        .config(function ($routeProvider) {
+
+            $routeProvider.when("/complete", {
+                templateUrl: "/views/thankYou.html"
+            });
+
+            $routeProvider.when("/placeorder", {
+                templateUrl: "/views/placeOrder.html"
+            });
+
+            $routeProvider.when("/checkout", {
+                templateUrl: "/views/checkoutSummary.html"
+            });
+
+            $routeProvider.when("/products", {
+                templateUrl: "/views/productList.html"
+            });
+
+            $routeProvider.otherwise({
+                templateUrl: "/views/productList.html"
+            });
+        });
+
+angular.module("sportsStore")
     .constant("dataUrl", "http://localhost:2403/products")
     .controller("sportsStoreCtrl", function ($scope, $http, dataUrl) {
 
